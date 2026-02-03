bad32eff476efe23ea86c6f15c4bff99de9b6670
Sebls <slopezsi@unal.edu.co>
2026-02-03 21:24:37 +0100

    feat(main): include plants router in the FastAPI application
    - Integrated the plants router into the main application to enable plant management endpoints.
    - Enhanced the application structure by organizing API routes effectively.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
+from app.api.routers import plants
 
 
 def create_app() -> FastAPI:
     app = FastAPI(title="smart-irrigation-api")
 
+    app.include_router(plants.router)
+
     return app
 
 
 app = create_app()
-
